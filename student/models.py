1e78b7a8f4b5df23abf1374f4348e2a945b5adce
pritam2317 <pritsigh2317@gmail.com>
2023-01-27 01:38:08 +0530

    create_studentmodel

--- student/models.py
+++ student/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class student(models.Model):
+    student_id = models.CharField(max_length=20,primary_key=True)
+    student_name = models.CharField(max_length=30)
+    student_class = models.IntegerField()
+    student_roll = models.IntegerField()
+    student_image = models.ImageField(upload_to='upload to/')
+    student_username = models.EmailField()
+    student_password = models.CharField
